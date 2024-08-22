579ababf2767995279d4d9c1f8de02638c1538dc
maundu55 <marymaundu.mm@gmail.com>
2024-08-22 11:13:19 +0300

    add delete task functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const textInput = document.getElementById("text-input");
 const addBtn = document.getElementById("add-btn");
-const listEl = document.getElementById("list")
+const listEl = document.getElementById("list");
+const listItem = document.getElementById("list-item");
+
 
 function addTask(){
     const task =textInput.value.trim();
@@ -8,10 +10,31 @@ function addTask(){
     console.log(textInput.value);
     textInput.value = "";
     }
-    
+const li = document.createElement('li');
+   li.textContent = task;
+   listEl.appendChild(li);
+    console.log(task)
+    const deleteBtn = document.createElement('button');
+deleteBtn.classList.add('delete-btn');
+deleteBtn.type = "button";
+deleteBtn.innerText = "Delete";
+li.appendChild(deleteBtn);
 }
 addBtn.addEventListener('click', addTask);
-
-function displayTasks(){
-    
+textInput.addEventListener('keypress', (e) =>
+{
+if(e.key=="Enter"){
+    addTask();
 }
+});
+
+function deleteTasks(id){
+    deleteBtn.addEventListener('click', (id)=>{
+        if(li.id===id){
+            listEl.removeChild(li);
+            console.log('clicked')   
+        }
+       
+    })
+}
+
